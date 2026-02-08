835b6d0e6e19f8109880615f512d59469bb41c30
aruunkumar <38814086+aruunkumar@users.noreply.github.com>
2026-02-07 22:24:03 -0500

    Remove unwanted table

--- modules/dynamodb/main.tf
+++ modules/dynamodb/main.tf
@@ -16,25 +16,3 @@ resource "aws_dynamodb_table" "main_table" {
     type = "S"
   }
 }
-
-# resource "aws_dynamodb_table" "chat_history_table" {
-#   name         = var.conversation_history_table_name
-#   billing_mode = "PAY_PER_REQUEST"
-#   hash_key     = "conversation_id"
-#   range_key    = "user_id"
-  
-#   ttl {
-#     attribute_name = "conversation_age"
-#     enabled        = true
-#   }
-
-#   attribute {
-#     name = "conversation_id"
-#     type = "S"
-#   }
-
-#   attribute {
-#     name = "user_id"
-#     type = "N"
-#   }
-# }
